e8b44b3c81e2d6f2759862710beebe5563b15149
Kirin <kirinmurphy@gmail.com>
2020-09-28 12:12:57 -0500

    calling useUrl param from codethings-nextjs-router-addons

--- utils/useCuisineFilter.ts
+++ utils/useCuisineFilter.ts
@@ -1,23 +1,23 @@
-import { ClearParamType, UpdateParamType, useUrlParam } from './useUrlParam';
+import { ClearParamType, UpdateParamType, useUrlParam } from 'codethings-nextjs-router-addons';
 
-const BITE_TYPE_PARAM = 'cuisine';
+const CUISINE_TYPE_PARAM = 'cuisine';
 
 interface ReturnProps {
-  cuisineTypeFromUrl: string;
+  cuisineTypeFromUrl: string | string[];
   updateCuisineType: UpdateParamType;
   clearCuisineType: ClearParamType;
 }
 
 export function useCuisineFilter (): ReturnProps {
   const { 
-    paramValueFromUrl, 
-    updateParam, 
-    clearParam, 
-  } = useUrlParam(BITE_TYPE_PARAM);
+    paramValue: cuisineTypeFromUrl, 
+    updateParam: updateCuisineType, 
+    clearParam: clearCuisineType, 
+  } = useUrlParam(CUISINE_TYPE_PARAM);
 
   return {
-    cuisineTypeFromUrl: paramValueFromUrl,
-    updateCuisineType: updateParam,
-    clearCuisineType: clearParam
+    cuisineTypeFromUrl,
+    updateCuisineType,
+    clearCuisineType
   };
 }
