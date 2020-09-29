e426eed857a72408fc797e3256319406d131d50f
Kirin <kirinmurphy@gmail.com>
2020-09-28 22:32:56 -0500

    converted query partial to fragment

--- apollo/queries/bite/bites.ts
+++ apollo/queries/bite/bites.ts
@@ -1,41 +1,37 @@
 import gql from "graphql-tag";
+import { GQL_FRAGMENT_SOURCES } from "../source/sources";
 
-const biteContent = `
-  {
+const GQL_FRAGMENT_BITES = gql`
+  fragment BiteContent on Bite {
     id
     name
     photo {
       url
     }
-    source {
-      id
-      name
-      location {
-        latitude
-        longitude
-        neighborhood
-      }
-    }
+    source { ...SourceContent }
   }
+  ${GQL_FRAGMENT_SOURCES}
 `;
 
 export const BITE_BY_ID = gql`
   query Bite($id: ID!){
-    bite(id: $id) {
-      id
-      name
-    } 
+    bite(id: $id) { ...BiteContent } 
   }
+  ${GQL_FRAGMENT_BITES}
 `;
 
 export const FILTERED_BITE_QUERY = gql`
   query Bites($cuisineName: String!) {
-    bites(where: { cuisines: { name: $cuisineName } }) ${biteContent}  
+    bites(where: { cuisines: { name: $cuisineName } }) {
+      ...BiteContent
+    }  
   }
+  ${GQL_FRAGMENT_BITES}
 `;
 
 export const BITE_QUERY = gql`
   query Bites { 
-    bites ${biteContent} 
+    bites { ...BiteContent }
   }
+  ${GQL_FRAGMENT_BITES}
 `;
--- apollo/queries/source/sources.ts
+++ apollo/queries/source/sources.ts
@@ -1,17 +1,22 @@
 import gql from "graphql-tag";
 
-const SOURCES_QUERY = gql`
-  query Sources {
-    sources {
-      id
-      name
-      location {
-        latitude
-        longitude
-        neighborhood
-      }
+export const GQL_FRAGMENT_SOURCES = gql`
+  fragment SourceContent on Source {
+    id
+    name
+    location {
+      latitude
+      longitude
+      neighborhood
     }
   }
+`;
+
+const SOURCES_QUERY = gql`
+  query Sources {
+    sources { ...SourceContent }
+  }
+  ${GQL_FRAGMENT_SOURCES}
 `;
 
 export default SOURCES_QUERY;
