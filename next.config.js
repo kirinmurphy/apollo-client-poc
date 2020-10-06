9ef5614d00895f78136bfd32d070c2a3c724838d
Kirin <kirinmurphy@gmail.com>
2020-10-06 18:29:08 -0500

    next-on-netlify-ing

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 // const withFonts = require("next-fonts");
 
 module.exports = {
+  target: "serverless",
   env: {
     API_URL: process.env.API_URL,
     IMAGE_ASSET_URL: process.env.IMAGE_ASSET_URL || ''
